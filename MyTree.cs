52b14bdf07a9d105bb4f6360d96add2c48d6f8b3
Uliana Degtiareva <uvdegtiareva@edu.hse.ru>
2024-05-08 12:33:36 +0500

    Добавьте файлы проекта.

---
+++ MyTree.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryLaba10;
+
+namespace laba12_3
+{
+    public class MyTree<T> where T : IInit, IComparable, ICloneable, new()
+    {
+        Point<T>? root = null;
+
+        public Point<T>? Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+
+        int count = 0;
+
+        public int Count => count;
+
+        public MyTree(int lenght)
+        { 
+            count = lenght;
+            root = MakeTree(lenght);
+        }
+
+        public MyTree() { }
+
+        public void ShowTree()
+        {
+            Show(root);
+        }
+
+        //ИСД
+        Point<T> MakeTree(int length)
+        {
+            if (length == 0)
+            {
+                return null;
+            }
+
+            // Определение разделения элементов на левое и правое поддеревья
+            int nleft = (length - 1) / 2;
+            int nright = length - nleft - 1;
+
+            // Создание нового корневого элемента
+            T data = new T();
+            data.RandomInit();
+            Point<T> newItem = new Point<T>(data);
+
+            // Рекурсивное создание левого и правого поддеревьев
+            newItem.Left = MakeTree(nleft);
+            newItem.Right = MakeTree(nright);
+
+            return newItem;
+        }
+
+        // Рекурсивный метод для вывода дерева на консоль
+        void Show(Point<T>? point, int spaces = 5)
+        {
+            if (point != null)
+            {
+                Show(point.Left, spaces + 5);
+                for (int i = 0; i < spaces; i++) 
+                    Console.Write("  ");
+                Console.WriteLine(point.Data);
+                Show(point.Right, spaces + 5);
+            }
+        }
+
+        //дерево поиска
+        public void AddPoint(T dataold)
+        {
+            T data = (T)dataold.Clone();
+            Point<T>? point = root;
+            Point<T>? current = null;
+            bool isEx = false;
+
+            while (point != null && !isEx)
+            {
+                current = point;
+
+                if (point.Data.CompareTo(data) == 0) // Проверка наличия элемента
+                {
+                    isEx = true;
+                }
+                else
+                {
+                    if (point.Data.CompareTo(data) > 0)
+                    {
+                        point = point.Left;
+                    }
+                    else
+                    {
+                        point = point.Right;
+                    }
+                }
+            }
+
+            // Если элемент уже существует, просто выходим
+            if (isEx) return;
+
+            Point<T> newPoint = new Point<T>(data);
+            if (current.Data.CompareTo(data) > 0) current.Left = newPoint;
+            else current.Right = newPoint;
+           
+            count++;
+        }
+
+        // Преобразование дерева в массив для последующего поиска
+        public void TransformToArray(Point<T>? point, T[] arr, ref int current)
+        {
+            if (point != null)
+            {
+                TransformToArray(point.Left, arr, ref current);
+                arr[current++] = point.Data; // Увеличиваем current после присвоения
+                TransformToArray(point.Right, arr, ref current);
+            }
+        }
+
+        public MyTree<T> TransformToFindTree()
+        {
+            if (root == null || count == 0)
+            {
+                throw new Exception("Дерево пустое. Перед продолжением сгенерируйте идеально сбалансированное дерево");
+            }
+            else
+            {
+                // Создание нового дерева для поиска
+                T[] arr = new T[count];
+                int current = 0;
+                TransformToArray(root, arr, ref current);
+
+                // Создание нового дерева поиска с корнем из идеально сбалансированного дерева
+                MyTree<T> tree = new MyTree<T>();
+                int mid = (arr.Length - 1) / 2;
+                tree.Root = new Point<T>(arr[mid]);
+                tree.count = 1;
+
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    tree.AddPoint(arr[i]);
+                }
+                return tree;
+            }
+        }
+
+
+
+        public void Clear()
+        {
+            ClearMemory(root);
+            root = null;
+            count = 0;
+        }
+
+        void ClearMemory(Point<T>? node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            ClearMemory(node.Left);
+            ClearMemory(node.Right);
+
+            // Удаление узла из памяти
+            node.Data = default; // Сброс данных узла
+            node.Left = null;
+            node.Right = null;
+        }
+
+        public double CalculateAverageNameLength()
+        {
+
+            int totalLength = 0;
+            int totalCount = 0;
+
+            CalculateNameLength(Root, ref totalLength, ref totalCount);
+
+            if (totalCount == 0)
+            {
+                return 0;
+            }
+
+            return (double)totalLength / totalCount;
+        }
+
+        public void CalculateNameLength(dynamic node, ref int totalLength, ref int totalCount)
+        {
+            if (node != null)
+            {
+                if (node.Data != null && node.Data.Name != null)
+                {
+                    string name = node.Data.Name;
+                    totalLength += name.Length;
+                    totalCount++;
+                }
+
+                CalculateNameLength(node.Left, ref totalLength, ref totalCount);
+                CalculateNameLength(node.Right, ref totalLength, ref totalCount);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryLaba10;
+
+namespace laba12_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTree<Plants> tree = new MyTree<Plants>();
+            MyTree<Plants> plant = new MyTree<Plants>();
+            int answer;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("------------------ РАБОТА С БИНАРНЫМ ДЕРЕВОМ -----------------");
+                Console.WriteLine("1. Создание идеально сбалансированного дерева");
+                Console.WriteLine("2. Печать идеально сбалансированного дерева");
+                Console.WriteLine("3. Вычисление средней длины названий элементов");
+                Console.WriteLine("4. Создание дерева поиска из идеально сбалансированного дерева");
+                Console.WriteLine("5. Печать дерева поиска");
+                Console.WriteLine("6. Удаление дерева из памяти");
+                Console.WriteLine("7. Выход");
+                Console.WriteLine();
+                answer = InputAnswer();
+                Console.WriteLine();
+                switch (answer)
+                {
+                    case 1: tree = Сreating(); break;
+                    case 2: Print(tree); break;
+                    case 3: AverageName(tree); break;
+                    case 4:
+                        {
+                            if (tree.Count == 0)
+                            {
+                                Console.WriteLine("Перед продолжением необходимо создать идеально сбалансированное дерево");
+                            }
+                            else
+                            {
+                                plant = tree.TransformToFindTree();
+                                Console.WriteLine("Дерево поиска создано");
+                            }
+                            break;
+                        }
+                    case 5: PrintFindTree(plant); break;
+                    case 6:
+                        if (plant.Count == 0)
+                        {
+                            Console.WriteLine("Дерево поиска не создано");
+                        }
+                        else 
+                        {
+                            plant.Clear();
+                            Console.WriteLine("Дерево поиска удалено");
+                        }
+                        if (tree.Count == 0)
+                        {
+                            Console.WriteLine("Идеально сбалансированное дерево не создано");
+                        }
+                        else
+                        {
+                            tree.Clear();
+                            Console.WriteLine("Идеально сбалансированное дерево удалено");
+                        }
+                        break;
+                    case 7: break;
+                    default:
+                        {
+                            Console.WriteLine("Неправильно задан пункт меню");
+                            break;
+                        }
+                }
+            } while (answer != 7);
+            Console.ReadLine();
+        }
+
+        static int InputAnswer()
+        {
+            int answer;
+            bool Ok;
+            do
+            {
+                string buf = Console.ReadLine();
+                Ok = int.TryParse(buf, out answer);
+                if (!Ok)
+                {
+                    Console.WriteLine("Неправильно выбран пункт меню. Повторите ввод");
+                }
+            } while (!Ok);
+            return answer;
+        }
+        static MyTree<Plants> Сreating()
+        {
+            int size;
+            bool oK;
+            do
+            {
+                Console.WriteLine("Введите размер списка:");
+                string buf = Console.ReadLine();
+                oK = int.TryParse(buf, out size);
+                if ((!oK) || (size <= 0))
+                {
+                    Console.WriteLine("Неверный ввод количества элементов. Повторите ввод");
+                }
+            } while ((!oK) || (size <= 0));
+            MyTree<Plants> tree = new MyTree<Plants>(size);
+            Console.WriteLine("Дерево создано");
+            return tree;
+        }
+        static void Print(MyTree<Plants> tree)
+        {
+            if (tree.Count == 0)
+            {
+                Console.WriteLine("Перед продолжением необходимо создать идеально сбалансированное дерево");
+            }
+            else 
+            {
+                Console.WriteLine("ИДЕАЛЬНО СБАЛАНСИРОВАННОЕ ДЕРЕВО: ");
+                tree.ShowTree();
+            }
+        }
+        static void AverageName(MyTree<Plants> tree)
+        {
+            if (tree.Count == 0)
+            {
+                Console.WriteLine("Дерево пустое, вычисление невозможно. Перед продолжением необходимо создать дерево");
+            }
+            else
+            {
+                double height = tree.CalculateAverageNameLength();
+                Console.WriteLine($"Средняя длина всех названий элементов равна = {height}");
+            }
+        }
+        static void PrintFindTree(MyTree<Plants> tree)
+        {
+            if (tree.Count == 0)
+            {
+                Console.WriteLine("Перед продолжением необходимо создать дерево поиска");
+            }
+            else
+            {
+                Console.WriteLine("ДЕРЕВО ПОИСКА: ");
+                tree.ShowTree();
+            }
+        }
+
+    }
+}
